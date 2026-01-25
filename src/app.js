c9ec3ba55a9ee3dd4fb676c7c0cfe5d05d5e085d
kapil desh <kapildesh87@gmail.com>
2026-01-25 15:42:26 +0530

    Multiple-Route-Handlers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log("hi kapil...");
+const express = require('express');
+
+const app = express();
+
+app.get("/user",
+
+(req,res,next)=>{
+    console.log("Response 1");
+    next();
+},
+(req,res,next) => {
+
+    console.log("Response 2");
+    next()
+
+},
+(req,res,next)=>{
+    console.log("Response 3");  
+    res.send("Response 3");
+}
+)
+
+
+
+app.listen(3000,()=>{
+     console.log("Server is Listening on Port 3000...");  
+})
+
+
