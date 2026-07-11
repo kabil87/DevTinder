864209387fb1b835325cc2d9137c114a19fdf2bb
kabil87 <kabildesh87@gmail.com>
2026-07-12 01:08:06 +0530

    ch

--- src/Config/database.js
+++ src/Config/database.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
 
+    console.log("Debugking...");
+    
+    console.log(process.env.DB_CONNECTION);
+    
     await mongoose.connect(process.env.DB_CONNECTION);
 };
 module.exports = connectDb;
